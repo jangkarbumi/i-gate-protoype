b61cc71c5573d6f1c15380d7e290829a444f290c
jangkarbumi <wahyuuajigtn13@gmail.com>
2025-09-20 21:25:37 +0700

    Adding express.js 4 and server.js

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.get("/api/custom", (req, res) => {
+    res.json({ message: "test jir" });
+  });
+
+
+  server.all("*", (req, res) => {
+    return handle(req, res);
+  });
+
+  const port = process.env.PORT || 3000;
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`🚀 Server running at http://localhost:${port}`);
+  });
+});
